99280d1168034c216a1f572bb618d7d46efb7472
ShibaevFntk <ShibaevFntk@gmail.com>
2025-08-17 05:36:48 +0300

    JUnit5 Testing

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void CashbackHackServiceRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+
+}
