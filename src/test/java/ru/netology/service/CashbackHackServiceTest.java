725527a3a7a07e5390bb7c97f226f65a17a2880d
ShibaevFntk <ShibaevFntk@gmail.com>
2025-08-17 06:02:03 +0300

    gUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,20 +1,39 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
-    @Test
-    void CashbackHackServiceRemain() {
+    @org.junit.Test
+    public void CashbackHackServiceRemainMax() {
         CashbackHackService service = new CashbackHackService();
         int amount = 999;
 
         int actual = service.remain(amount);
         int expected = 1;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
+    @org.junit.Test
+    public void CashbackHackServiceRemainMin() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
 
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void CashbackHackServiceRemainEqual() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
